cd300654423f9cb91fcc5ae5a24736f908050535
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-03-18 14:22:46 -0400

    ...

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let map = {
-  "pie": "/pietrainingguide",
-  "malla": "/malltrainingguide",
-  "yandrak": "/yandraktrainingguide",
-  "joni": "/jonitrainingguide"
-};
+// let map = {
+//   "pie": "/pietrainingguide",
+//   "malla": "/malltrainingguide",
+//   "yandrak": "/yandraktrainingguide",
+//   "joni": "/jonitrainingguide"
+// };
 
-let newUserMap = {
-  "john": "/johntrainingguide",
-  "jane": "/janetrainingguide"
-};
+// let newUserMap = {
+//   "john": "/johntrainingguide",
+//   "jane": "/janetrainingguide"
+// };
 
-var form = document.querySelector("form");
-if (form) {
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var dropdown = document.querySelector("#dropdown");
-    var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-    if (selectedOption in map) {
-      window.location.href = map[selectedOption];
-    } else if (selectedOption in newUserMap) {
-      window.location.href = newUserMap[selectedOption];
-    }
-  });
-}
+// var form = document.querySelector("form");
+// if (form) {
+//   form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     var dropdown = document.querySelector("#dropdown");
+//     var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+//     if (selectedOption in map) {
+//       window.location.href = map[selectedOption];
+//     } else if (selectedOption in newUserMap) {
+//       window.location.href = newUserMap[selectedOption];
+//     }
+//   });
+// }
